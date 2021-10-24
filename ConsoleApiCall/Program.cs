34fca7a8f66017b0ce60c06e4057b17263d7a18a
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-10-24 15:27:17 -0700

    add async task and json conversion
    Co-authored-by: Jessica R. Williams <jessicarubinwilliams@gmail.com>
    Co-authored-by: Alex Bertotto <ajb5206@gmail.com>

---
+++ ConsoleApiCall/Program.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ApiTest
+{
+    class Program
+    {
+        static void Main()
+        {
+            var apiCallTask = ApiHelper.ApiCall("U5EwX8GOtCxgtrrdRxSnuU4yUrBZKB6w");
+            var result = apiCallTask.Result;
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+            List<Article> articleList = JsonConvert.DeserializeObject<List<Article>>(jsonResponse["results"].ToString());
+
+                foreach (Article article in articleList)
+                {
+                    Console.WriteLine($"Section: {article.Section}");
+                    Console.WriteLine($"Title: {article.Title}");
+                    Console.WriteLine($"Abstract: {article.Abstract}");
+                    Console.WriteLine($"Url: {article.Url}");
+                    Console.WriteLine($"Byline: {article.Byline}");
+                }
+        }
+
+    }
+    class ApiHelper 
+    {
+        public static async Task<string> ApiCall(string apiKey)
+        {
+            RestClient client = new RestClient("https://api.nytimes.com/svc/topstories/v2");
+            RestRequest request = new RestRequest($"home.json?api-key={apiKey}", Method.GET);
+            var response = await client.ExecuteTaskAsync(request);
+            return response.Content;
+        }
+    }
+}
